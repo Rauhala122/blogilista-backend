3bacf21e759b797a88978d9799c5499d275c81e5
Saska Rauhala <saskarauhala@MacBook-Pro-4.home>
2020-04-22 19:57:52 +0300

    blog app backend finished

---
+++ app.js
@@ -0,0 +1,26 @@
+const config = require("./utils/config")
+const express = require('express')
+require("express-async-errors")
+const app = express()
+const cors = require('cors')
+const blogsRouter = require("./controllers/blogs")
+const usersRouter = require("./controllers/users")
+const loginRouter = require("./controllers/login")
+const mongoose = require('mongoose')
+
+const mongoUrl = process.env.MONGODB_URI
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true})
+
+app.use(cors())
+app.use(express.json())
+app.use("/api/blogs", blogsRouter)
+app.use("/api/users", usersRouter)
+app.use("/api/login", loginRouter)
+
+
+const testingRouter = require('./controllers/testing')
+app.use('/api/testing', testingRouter)
+
+
+module.exports = app
---
+++ controllers/blogs.js
@@ -0,0 +1,96 @@
+const blogsRouter = require("express").Router()
+const Blog = require("../models/blog")
+const User = require("../models/user")
+const jwt = require('jsonwebtoken')
+const Comment = require("../models/comment")
+
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1}).populate("comments")
+  response.json(blogs.map(blog => blog.toJSON()))
+})
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
+blogsRouter.post('/', async (request, response) => {
+  const body = request.body
+
+  let likes
+
+  const token = getTokenFrom(request)
+
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+
+  if (body.likes) {
+    likes = body.likes
+  } else {
+    likes = 0
+  }
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: likes,
+    user: user._id
+  })
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+  response.json(savedBlog.toJSON())
+})
+
+blogsRouter.get("/:id", async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  response.json(blog.toJSON())
+})
+
+blogsRouter.delete("/:id", async (request, response) => {
+
+  const blog = await Blog.findById(request.params.id)
+
+  const token = getTokenFrom(request)
+
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || decodedToken.id !== blog.user.toString() || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid for deleting blog' })
+  }
+
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put("/:id", async (request, response) => {
+  const body = request.body
+
+  console.log(body)
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, body, { new: true })
+  response.json(updatedBlog.toJSON())
+})
+
+blogsRouter.post("/:id/comments", async (request, response) => {
+  const body = request.body
+  const blog = await Blog.findById(request.params.id)
+  console.log(blog)
+
+  const comment = new Comment(body)
+  const savedComment = await comment.save()
+  console.log(savedComment)
+  blog.comments = blog.comments.concat(savedComment._id)
+  await blog.save()
+  response.json(savedComment.toJSON())
+})
+
+module.exports = blogsRouter
